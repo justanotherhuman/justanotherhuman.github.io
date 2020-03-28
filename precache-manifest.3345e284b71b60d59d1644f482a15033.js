5ce4b174537ce09f3942ff9319c2b8436e8b54ff
Justan Human <justanhuman@Justans-MacBook-Pro.local>
2020-03-27 23:13:07 -0500

    Updates

---
+++ precache-manifest.3345e284b71b60d59d1644f482a15033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968495a2b8cb92eb1eab1e83eaa9b1a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6fb046fc4c278469324",
+    "url": "/static/css/main.7d70d848.chunk.css"
+  },
+  {
+    "revision": "31f9734e3b09fc87124e",
+    "url": "/static/js/2.de01d556.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.de01d556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6fb046fc4c278469324",
+    "url": "/static/js/main.ecdd6be4.chunk.js"
+  },
+  {
+    "revision": "c5647c151d886fed45d2",
+    "url": "/static/js/runtime-main.201927c6.js"
+  }
+]);
